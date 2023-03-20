20a1f7bb459698a5fab7efb25a7c09944e0d24e7
Justin Ross <jross@apache.org>
2023-03-20 09:39:36 -0400

    WIP

--- irc-server/.plano.py
+++ irc-server/.plano.py
@@ -11,6 +11,16 @@ def run_():
     build()
     run(f"podman run --rm -p 6667:6667 {image_tag}")
 
+# docker run -d \
+#   --name=ngircd \
+#   -e PUID=1000 \
+#   -e PGID=1000 \
+#   -e TZ=Etc/UTC \
+#   -p 6667:6667 \
+#   -v /path/to/ngircd/config:/config \
+#   --restart unless-stopped \
+#   lscr.io/linuxserver/ngircd:latest
+
 @command
 def login():
     run("podman login quay.io")
